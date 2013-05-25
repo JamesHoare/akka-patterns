e58f68e284e6a88053e2827e107ff703702b7cf2
Jan Machacek <janm@cakesolutions.net>
2013-05-25 13:58:33 +0100

    Xuggler

--- server/main/src/main/scala/org/eigengo/akkapatterns/main/VideoDemo.scala
+++ server/main/src/main/scala/org/eigengo/akkapatterns/main/VideoDemo.scala
@@ -1,5 +1,7 @@
 package org.eigengo.akkapatterns.main
 
+import com.xuggle.xuggler.{IVideoPicture, IPacket, IMetaData, IContainer}
+
 
 /**
  * @author janmachacek
@@ -7,7 +9,18 @@ package org.eigengo.akkapatterns.main
 object VideoDemo {
 
   def main(args: Array[String]) {
+    val container = IContainer.make()
+    container.open("/Users/janmachacek/foo.mov", IContainer.Type.READ, null)
+    val videoStream = container.getStream(0)
+    val videoCoder = videoStream.getStreamCoder
+    videoCoder.open(IMetaData.make(), IMetaData.make())
 
+    val packet = IPacket.make()
+    while (container.readNextPacket(packet) >= 0) {
+      val picture = IVideoPicture.make(videoCoder.getPixelType, videoCoder.getWidth, videoCoder.getHeight)
+      packet.getSize
+
+    }
   }
 
 }
